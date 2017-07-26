3cec1feaa9f650f7d20db66305304cf1ffd21b78
Timothy Browne <timbrowne066@gmail.com>
2017-07-26 14:58:29 -0400

    fixed bug with blackjack conditional

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,57 +22,55 @@ end
 player.calculate_score
 if player.calculate_score == 21
   puts "BLACKJACK!!!"
-end
-
-if player.calculate_score < 21
+elsif player.calculate_score < 21
   puts "YOUR SCORE: #{player.calculate_score}\n\n"
-end
-
-puts "DEALER HAS:\n"
-2.times do computer.cards << deck.deal!
-end
-puts computer.cards[-1].suit + computer.cards[-1].value
-puts "\n"
-
-puts "Press 'h' to hit or 's' to stay.\n"
-input = gets.chomp
-
-while input == 'h' do
-  player.cards << deck.deal!
-  puts player.cards[-1].suit + player.cards[-1].value
-  player.calculate_score
-  if player.calculate_score > 21
-    puts "YOU BUSTED!"
-    break
-  else
-    puts "YOUR SCORE: #{player.calculate_score}\n\n"
-    puts "Press 'h' to hit or 's' to stay.\n"
-    input = gets.chomp
+  puts "DEALER HAS:\n"
+  2.times do computer.cards << deck.deal!
   end
-end
+  puts computer.cards[-1].suit + computer.cards[-1].value
+  puts "\n"
 
-if input == 's'
-  puts "YOUR TOTAL SCORE #{player.calculate_score}\n\n"
 
-  while computer.calculate_score < 17
-    computer.cards << deck.deal!
+  puts "Press 'h' to hit or 's' to stay.\n"
+  input = gets.chomp
+
+  while input == 'h' do
+    player.cards << deck.deal!
+    puts player.cards[-1].suit + player.cards[-1].value
+    player.calculate_score
+    if player.calculate_score > 21
+      puts "YOU BUSTED!"
+      break
+    else
+      puts "YOUR SCORE: #{player.calculate_score}\n\n"
+      puts "Press 'h' to hit or 's' to stay.\n"
+      input = gets.chomp
+    end
   end
 
-  puts "DEALER HAS"
-  computer.cards.each do |card|
-    puts card.suit + card.value
-  end
-  puts "\nDEALER SCORE: #{computer.calculate_score}\n\n"
+  if input == 's'
+    puts "YOUR TOTAL SCORE #{player.calculate_score}\n\n"
 
-  if player.calculate_score > 21 && computer.calculate_score <= 21
-    puts "YOU BUSTED!  DEALER WINS!"
-  elsif computer.calculate_score > 21 && player.calculate_score <= 21
-    puts "DEALER BUSTED!  YOU WIN!"
-  elsif player.calculate_score > computer.calculate_score && player.calculate_score <= 21
-    puts "YOU WIN!"
-  elsif computer.calculate_score > player.calculate_score && computer.calculate_score <= 21
-    puts "DEALER WINS!"
-  else
-    puts "PUSH!"
+    while computer.calculate_score < 17
+      computer.cards << deck.deal!
+    end
+
+    puts "DEALER HAS"
+    computer.cards.each do |card|
+      puts card.suit + card.value
+    end
+    puts "\nDEALER SCORE: #{computer.calculate_score}\n\n"
+
+    if player.calculate_score > 21 && computer.calculate_score <= 21
+      puts "YOU BUSTED!  DEALER WINS!"
+    elsif computer.calculate_score > 21 && player.calculate_score <= 21
+      puts "DEALER BUSTED!  YOU WIN!"
+    elsif player.calculate_score > computer.calculate_score && player.calculate_score <= 21
+      puts "YOU WIN!"
+    elsif computer.calculate_score > player.calculate_score && computer.calculate_score <= 21
+      puts "DEALER WINS!"
+    else
+      puts "PUSH!"
+    end
   end
 end
